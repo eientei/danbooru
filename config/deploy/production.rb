f2901e27c970ad286fa198f65451cd0787bd28f0
Alexander Tumin <iamtakingiteasy@eientei.org>
2019-05-10 12:41:15 +0300

    up

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "localhost", :roles => %w(web app db), :primary => true
+server "localhost", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
